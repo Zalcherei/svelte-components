48c4e7aaa4e46e036d3e19b5b4c0c13cfd3ca108
Zalcherei <Zalcherei@github.com>
2024-07-15 22:41:36 +0300

    Updates

---
+++ _app/immutable/chunks/paths.D15ra5kt.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_zkhvjl)==null?void 0:s.base)??"/svelte-components";var e;const a=((e=globalThis.__sveltekit_zkhvjl)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.Ba4OZCyy.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CzMHVuC-.js";export{t as start};

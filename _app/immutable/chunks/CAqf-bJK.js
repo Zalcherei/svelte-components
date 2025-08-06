b3f81680ed1b0e948baa2e9b7f2c053ab890f4ff
Zalcherei <toni.siuro@outlook.com>
2025-08-06 17:51:28 +0300

    Updates

---
+++ _app/immutable/chunks/CAqf-bJK.js
@@ -0,0 +1,1 @@
+import{V as a}from"./xfZ-T3O0.js";a();
---
+++ _app/immutable/chunks/DbNMWoy8.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ksil3v?.base??"/svelte-components",e=globalThis.__sveltekit_ksil3v?.assets??s;export{e as a,s as b};
